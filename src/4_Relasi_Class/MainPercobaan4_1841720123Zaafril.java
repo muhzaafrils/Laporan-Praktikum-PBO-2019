0bb679951542eb2f13ba8e66ab31e3aaa0359a77
muhzaafrils <54797796+muhzaafrils@users.noreply.github.com>
2019-09-22 14:43:40 +0700

    Update MainPercobaan4_1841720123Zaafril.java

--- src/4_Relasi_Class/MainPercobaan4_1841720123Zaafril.java
+++ src/4_Relasi_Class/MainPercobaan4_1841720123Zaafril.java
@@ -11,13 +11,13 @@ package minggu4_p4;
  */
 public class MainPercobaan4_1841720123Zaafril {
     public static void main(String[] args){
-    Penumpang1841720123Zaafril p = new Penumpang1841720123Zaafril("12345", "Mr. Krab");
-    Gerbong1841720123Zaafril gerbong = new Gerbong1841720123Zaafril("A",10);
-    gerbong.setPenumpangZaafril(p, 1);
-    System.out.println(gerbong.infoZaafril());
-    Penumpang1841720123Zaafril budi = new Penumpang1841720123Zaafril("678910", "budi");
-    Gerbong1841720123Zaafril gerbong2 = new Gerbong1841720123Zaafril("B",15);
-    gerbong.setPenumpangZaafril(budi, 2);
-    System.out.println(gerbong.infoZaafril());
+        Penumpang1841720123Zaafril p = new Penumpang1841720123Zaafril("12345", "Mr. Krab");
+        Gerbong1841720123Zaafril gerbong = new Gerbong1841720123Zaafril("A",10);
+        gerbong.setPenumpangZaafril(p, 1);
+        System.out.println(gerbong.infoZaafril());
+        Penumpang1841720123Zaafril budi = new Penumpang1841720123Zaafril("678910", "budi");
+        Gerbong1841720123Zaafril gerbong2 = new Gerbong1841720123Zaafril("B",15);
+        gerbong.setPenumpangZaafril(budi, 2);
+        System.out.println(gerbong.infoZaafril());
     }
 }
