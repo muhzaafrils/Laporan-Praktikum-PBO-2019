c4c066a8d49f5aea2855b2ef06032979cfa8e3e6
muhzaafrils <54797796+muhzaafrils@users.noreply.github.com>
2019-10-06 19:16:29 +0700

    Add files via upload

---
+++ src/6_Inheritance/minggu6_P4/ClassC1841720123Zaafril.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu6_P4;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class ClassC1841720123Zaafril extends ClassB1841720123Zaafril{
+    ClassC1841720123Zaafril(){
+        System.out.println("konstruktor C dijalankan");
+    }
+}
---
+++ src/6_Inheritance/minggu6_P4/Percobaan4_1841720123Zaafril.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu6_P4;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Percobaan4_1841720123Zaafril {
+    public static void main(String[] args){
+        ClassC1841720123Zaafril test = new ClassC1841720123Zaafril();  
+    }
+}
